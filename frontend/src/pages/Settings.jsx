b0d4aa69fd8f433f4376f2a95e2c3f6f7bae767b
Ashu463 <ashukasaudhan971@gmail.com>
2026-04-03 11:31:09 +0530

    fixed be urls

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useTheme } from '../context/ThemeContext';
 import axios from 'axios';
 
-const API_BASE = 'https://tleeleminatorsbackend.vercel.app';
+const API_BASE = 'https://tle-eleminators-backend.vercel.app/';
 
 const Settings = () => {
   const { isDark } = useTheme();
